90af49d8dd15f7db1c22200d5d9fd9fd3074b792
justina6700 <agustinamicaelagarcia@gmail.com>
2023-08-13 23:46:16 -0300

    Agregué eventos.js y styles.css

---
+++ eventos.js
@@ -0,0 +1,11 @@
+
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    const divv = document.getElementById('undiv');
+  
+    divv.addEventListener('click', function () {
+      alert('Hola! Soy el div');
+    });
+  });
+  
+  
