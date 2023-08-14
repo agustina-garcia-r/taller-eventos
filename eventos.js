00786c1d1583c4ed802e67bbe8de9c97dabb22c3
justina6700 <agustinamicaelagarcia@gmail.com>
2023-08-14 00:22:46 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,11 +1,26 @@
-
-  
-  document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     const divv = document.getElementById('undiv');
   
     divv.addEventListener('click', function () {
       alert('Hola! Soy el div');
     });
+  
+    const button = document.querySelector('button');
+  
+    button.addEventListener('click', function (event) {
+      event.stopPropagation(); // Detener la propagación del evento del botón
+      alert('Hola!'); // Alerta del botón
+    });
+  
+    // Agregar un nuevo listener para el evento click en el div que no propague
+    const divWithoutPropagation = document.getElementById('divWithoutPropagation');
+    
+    divWithoutPropagation.addEventListener('click', function (event) {
+      event.stopPropagation();
+      alert('Hola! Soy el div sin propagación');
+    });
   });
+  
+  
   
   
